0fff2957576a49dd5d0bb82b3ff40c48981b527a
ioiitic <96875097+ioiitic@users.noreply.github.com>
2022-01-18 20:13:36 +0700

    Add files via upload

--- List_Goods.h
+++ List_Goods.h
@@ -15,6 +15,7 @@ struct List_Goods
     List_Goods *next;
     void (*add)(List_Goods *obj, char *name, int num, char *unit);
     void (*delete)(List_Goods *father, List_Goods *obj);
+    void (*addalr)(List_Goods *obj, char *name, int num);
     void (*inp)(List_Goods *obj);
     void (*out)(List_Goods *obj);
     void (*destroy)(List_Goods *obj);
@@ -23,6 +24,7 @@ List_Goods *List_Goods_Init();
 List_Goods *Goods_Init(char *name, int num, char *unit);
 void Add_Goods(List_Goods *obj, char *name, int num, char *unit);
 void Delete_Goods(List_Goods *father, List_Goods *obj);
+void Addalr_Goods(List_Goods *obj, char *name, int num);
 void Inp_Goods(List_Goods *obj);
 void Out_Goods(List_Goods *obj);
 void Destroy_Goods(List_Goods *obj);
@@ -33,13 +35,14 @@ List_Goods *List_Goods_Init()
 {
     List_Goods* obj = (List_Goods*) malloc(sizeof(List_Goods));
     obj->next = NULL;
-    obj->name = malloc(sizeof(char));
-    obj->unit = malloc(sizeof(char));
+    obj->name = (char*)malloc(sizeof(char));
+    obj->unit = (char*)malloc(sizeof(char));
     obj->name = "";
     obj->num = 0;
     obj->unit = "";
     obj->add = &Add_Goods;
     obj->delete = &Delete_Goods;
+    obj->addalr = &Addalr_Goods;
     obj->inp = &Inp_Goods;
     obj->out = &Out_Goods;
     obj->destroy = &Destroy_Goods;
@@ -56,6 +59,7 @@ List_Goods *Goods_Init(char *name, int num, char *unit)
     _str(obj->unit, unit);
     obj->add = &Add_Goods;
     obj->delete = &Delete_Goods;
+    obj->addalr = &Addalr_Goods;
     obj->inp = &Inp_Goods;
     obj->out = &Out_Goods;
     obj->destroy = &Destroy_Goods;
@@ -75,6 +79,15 @@ void Delete_Goods(List_Goods *father, List_Goods *obj)
 {
     father->next = obj->next;
     free(obj);
+}
+void Addalr_Goods(List_Goods *obj, char *name, int num)
+{
+    List_Goods *list_goods = obj;
+    for(; list_goods != NULL; list_goods = list_goods->next)
+    {
+        if (list_goods->name == name) list_goods->num += num;
+        return;
+    }
 }
 void Inp_Goods(List_Goods *obj)
 {
@@ -100,19 +113,16 @@ void Inp_Goods(List_Goods *obj)
 void Out_Goods(List_Goods *obj)
 {
     FILE *out = fopen(fin_1, "w");
-    List_Goods *List_Goods = obj->next;
-    for(; List_Goods->next != NULL; List_Goods = List_Goods->next)
+    List_Goods *list_goods = obj->next;
+    for(; list_goods->next != NULL; list_goods = list_goods->next)
     {
-        fprintf(out, "%s,", List_Goods->name);
-        fprintf(out, "%d,", List_Goods->num);
-        fprintf(out, "%s,\n", List_Goods->unit);
-        printf("%s,", List_Goods->name);
-        printf("%d,", List_Goods->num);
-        printf("%s,\n", List_Goods->unit);
+        fprintf(out, "%s,", list_goods->name);
+        fprintf(out, "%d,", list_goods->num);
+        fprintf(out, "%s,\n", list_goods->unit);
     }
-    fprintf(out, "%s,", List_Goods->name);
-    fprintf(out, "%d,", List_Goods->num);
-    fprintf(out, "%s,", List_Goods->unit);
+    fprintf(out, "%s,", list_goods->name);
+    fprintf(out, "%d,", list_goods->num);
+    fprintf(out, "%s,", list_goods->unit);
     fclose(out);
 }
 void Destroy_Goods(List_Goods *obj)
